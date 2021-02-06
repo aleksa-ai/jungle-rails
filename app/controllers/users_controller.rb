a086dd4a4a73baf87b4306bbcff78aedcdb814bf
Aleksa Gavrilovic <lks_g@live.ca>
2021-02-06 00:26:08 -0500

    Fixing broken code & cleaning up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class NewController < ApplicationController
+class UsersController < ApplicationController
   def new
   end
 
@@ -15,6 +15,6 @@ class NewController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
